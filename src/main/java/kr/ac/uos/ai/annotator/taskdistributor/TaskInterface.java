b8c41b9c090520d37e8f6ff801402b37a61106aa
lovebube <lovebube@hotmail.com>
2015-10-02 18:44:00 +0900

    refactor proc 10-02-2015 18:43

---
+++ src/main/java/kr/ac/uos/ai/annotator/taskdistributor/TaskInterface.java
@@ -0,0 +1,10 @@
+package kr.ac.uos.ai.annotator.taskdistributor;
+
+/**
+ * @author Chan Yeon, Cho
+ * @version 0.0.1 - SnapShot
+ *          on 2015-10-02
+ */
+
+public interface TaskInterface {
+}
